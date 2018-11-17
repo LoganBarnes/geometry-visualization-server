a94c76ebf204c7ad3df14b33a39a62e2fcc7508e
Logan Barnes <logan@cloudnc.com>
2018-11-17 01:06:32 +0000

    chore(atomic-data-tests): Add tests and comments for AtomicData

--- src/gvs/common/atomic_data.hpp
+++ src/gvs/common/atomic_data.hpp
@@ -10,44 +10,86 @@
 namespace gvs {
 namespace util {
 
+/**
+ * @brief Owns complex data that can be accessed in a thread safe way.
+ *
+ * Example:
+ *
+ *     struct MyComplexData {
+ *         int thing1;
+ *         bool thing2;
+ *         std::vector<double> more_things;
+ *         OtherStruct complex_thing;
+ *     };
+ *
+ *     carpet::AtomicData<MyComplexData> shared_data;
+ *
+ *     ... Later, in different threads
+ *
+ *     shared_data.use_safely([] (MyComplexData& data) {
+ *         // Do things with 'data' here:
+ *         ...
+ *     });
+ *
+ *     ...
+ */
 template <typename T>
 class AtomicData {
 public:
     explicit AtomicData(T data = {});
 
+    /**
+     * @brief Use the data in a thread safe manner.
+     */
     template <typename Func>
     void use_safely(const Func& func);
 
     template <typename Func>
     void use_safely(const Func& func) const;
 
+    /**
+     * @brief Wait for 'notify_one' or 'notify_all' to be called on this data
+     *        structure before using the data in a thread safe manner.
+     */
     template <typename Pred, typename Func>
     void wait_to_use_safely(const Pred& predicate, const Func& func);
 
     template <typename Pred, typename Func>
     void wait_to_use_safely(const Pred& predicate, const Func& func) const;
 
+    /**
+     * @brief Same as 'wait_to_use_safely' except a maximum wait time can be set.
+     */
     template <typename Pred, typename Func>
     bool wait_to_use_safely(unsigned max_wait_time_millis, const Pred& predicate, const Func& func);
 
     template <typename Pred, typename Func>
     bool wait_to_use_safely(unsigned max_wait_time_millis, const Pred& predicate, const Func& func) const;
 
+    /**
+     * @brief Allow one 'wait_to_use_safely' function to continue.
+     */
     void notify_one();
 
+    /**
+     * @brief Allow all 'wait_to_use_safely' functions to continue.
+     */
     void notify_all();
 
+    /**
+     * @brief Access the data directly in a non thread safe manner
+     */
     T& unsafe_data();
     const T& unsafe_data() const;
 
 private:
-    std::unique_ptr<std::mutex> lock_;
+    mutable std::mutex lock_; // mutable so it can be used with const functions
     std::condition_variable condition_;
     T data_;
 };
 
 template <typename T>
-AtomicData<T>::AtomicData(T data) : lock_(std::make_unique<std::mutex>()), data_(std::move(data)) {}
+AtomicData<T>::AtomicData(T data) : data_(std::move(data)) {}
 
 template <typename T>
 T& AtomicData<T>::unsafe_data() {
@@ -62,21 +104,21 @@ const T& AtomicData<T>::unsafe_data() const {
 template <typename T>
 template <typename Func>
 void AtomicData<T>::use_safely(const Func& func) {
-    std::lock_guard<std::mutex> scoped_lock(*lock_);
+    std::lock_guard<std::mutex> scoped_lock(lock_);
     func(data_);
 }
 
 template <typename T>
 template <typename Func>
 void AtomicData<T>::use_safely(const Func& func) const {
-    std::lock_guard<std::mutex> scoped_lock(*lock_);
+    std::lock_guard<std::mutex> scoped_lock(lock_);
     func(data_);
 }
 
 template <typename T>
 template <typename Pred, typename Func>
 void AtomicData<T>::wait_to_use_safely(const Pred& predicate, const Func& func) {
-    std::unique_lock<std::mutex> unlockable_lock(*lock_);
+    std::unique_lock<std::mutex> unlockable_lock(lock_);
     condition_.wait(unlockable_lock, [&] { return predicate(data_); });
     func(data_);
 }
@@ -84,7 +126,7 @@ void AtomicData<T>::wait_to_use_safely(const Pred& predicate, const Func& func) 
 template <typename T>
 template <typename Pred, typename Func>
 void AtomicData<T>::wait_to_use_safely(const Pred& predicate, const Func& func) const {
-    std::unique_lock<std::mutex> unlockable_lock(*lock_);
+    std::unique_lock<std::mutex> unlockable_lock(lock_);
     condition_.wait(unlockable_lock, [&] { return predicate(data_); });
     func(data_);
 }
@@ -92,7 +134,7 @@ void AtomicData<T>::wait_to_use_safely(const Pred& predicate, const Func& func) 
 template <typename T>
 template <typename Pred, typename Func>
 bool AtomicData<T>::wait_to_use_safely(unsigned max_wait_time_millis, const Pred& predicate, const Func& func) {
-    std::unique_lock<std::mutex> unlockable_lock(*lock_);
+    std::unique_lock<std::mutex> unlockable_lock(lock_);
     if (condition_.wait_for(unlockable_lock, std::chrono::milliseconds(max_wait_time_millis), [&] {
             return predicate(data_);
         })) {
@@ -105,7 +147,7 @@ bool AtomicData<T>::wait_to_use_safely(unsigned max_wait_time_millis, const Pred
 template <typename T>
 template <typename Pred, typename Func>
 bool AtomicData<T>::wait_to_use_safely(unsigned max_wait_time_millis, const Pred& predicate, const Func& func) const {
-    std::unique_lock<std::mutex> unlockable_lock(*lock_);
+    std::unique_lock<std::mutex> unlockable_lock(lock_);
     if (condition_.wait_for(unlockable_lock, std::chrono::milliseconds(max_wait_time_millis), [&] {
             return predicate(data_);
         })) {
