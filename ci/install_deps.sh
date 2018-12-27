ea84e2ae11fdb5f145d39372735b30be3a74cd11
Logan Barnes <logan@cloudnc.com>
2018-12-27 12:38:45 -0700

    Attempt to use g++7 in ci pipeline

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -5,7 +5,10 @@ export PATH=/deps/cmake/bin:$PATH
 cmake --version
 
 # install deps
+apt update
 apt install -y software-properties-common # installs add-apt-repository
+
+# ppa for gcc-7
 add-apt-repository ppa:ubuntu-toolchain-r/test -y
 apt update
 apt install -y gcc-7 g++-7 xorg-dev libgl1-mesa-dev uuid-dev lcov
