42acff0093de30b8204fe2f25c3a5091f36f16d5
Logan Barnes <logan@cloudnc.com>
2018-12-27 02:41:27 -0700

    Attempt to run ci with docker image

--- ci/build_and_run.sh
+++ ci/build_and_run.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-#function build_and_run {
-#    cmake -E make_directory $1
-#    cmake -E chdir $1 cmake -DCMAKE_BUILD_TYPE=$2 -DGVS_USE_DEV_FLAGS=ON -DGVS_BUILD_TESTS=ON ..
-#    cmake -E chdir $1 cmake --build . --parallel
-#    ./$1/bin/tests/all_gvs_tests
-#}
+function build_and_run {
+    echo "Configuring"
+    cmake -E make_directory $1
+    cmake -E chdir $1 cmake -DCMAKE_BUILD_TYPE=$2 -DGVS_USE_DEV_FLAGS=ON -DGVS_BUILD_TESTS=ON ..
+    echo "Building"
+    cmake -E chdir $1 cmake --build . --parallel
+    echo "Running"
+    ./$1/bin/tests/all_gvs_tests
+}
+
+build_and_run cmake-build-debug Debug
+build_and_run cmake-build-release Release
+
+#mkdir cmake-build-debug
+#mkdir cmake-build-release
 #
-#build_and_run cmake-build-debug Debug
-#build_and_run cmake-build-release Release
-
-mkdir cmake-build-debug
-mkdir cmake-build-release
-
-pushd cmake-build-debug
-cmake -DCMAKE_BUILD_TYPE=Debug -DGVS_USE_DEV_FLAGS=ON -DGVS_BUILD_TESTS=ON ..
-popd
+#pushd cmake-build-debug
+#cmake -DCMAKE_BUILD_TYPE=Debug -DGVS_USE_DEV_FLAGS=ON -DGVS_BUILD_TESTS=ON ..
+#popd
