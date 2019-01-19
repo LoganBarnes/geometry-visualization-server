f9106695a8cef6c1acf9d068f43e4993a4bd3e1e
Logan Barnes <barneslt63@gmail.com>
2019-01-19 16:42:21 +0000

    Only run ci on ci branch now

--- src/testing/gvs_tests.cpp
+++ src/testing/gvs_tests.cpp
@@ -23,7 +23,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
-//#include "gvs/server/scene_server.hpp"
+#include "gvs/server/scene_server.hpp"
 
 #include "gvs/net/detail/async_rpc_handler_interface.hpp"
 #include "gvs/net/detail/non_stream_rpc_handler.hpp"
