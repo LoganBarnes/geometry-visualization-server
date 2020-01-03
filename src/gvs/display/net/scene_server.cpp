c0c194a5c32eeadbaac9aed12646d7dfe7a28278
Logan Barnes <barneslt63@gmail.com>
2020-01-03 00:53:03 +0000

    Make server its own service

--- src/gvs/display/net/scene_server.cpp
+++ src/gvs/display/net/scene_server.cpp
@@ -1,20 +1,29 @@
 // ///////////////////////////////////////////////////////////////////////////////////////
-//                                                                           |________|
-//  Copyright (c) 2020 CloudNC Ltd - All Rights Reserved                        |  |
-//                                                                              |__|
-//        ____                                                                .  ||
-//       / __ \                                                               .`~||$$$$
-//      | /  \ \         /$$$$$$  /$$                           /$$ /$$   /$$  /$$$$$$$
-//      \ \ \ \ \       /$$__  $$| $$                          | $$| $$$ | $$ /$$__  $$
-//    / / /  \ \ \     | $$  \__/| $$  /$$$$$$  /$$   /$$  /$$$$$$$| $$$$| $$| $$  \__/
-//   / / /    \ \__    | $$      | $$ /$$__  $$| $$  | $$ /$$__  $$| $$ $$ $$| $$
-//  / / /      \__ \   | $$      | $$| $$  \ $$| $$  | $$| $$  | $$| $$  $$$$| $$
-// | | / ________ \ \  | $$    $$| $$| $$  | $$| $$  | $$| $$  | $$| $$\  $$$| $$    $$
-//  \ \_/ ________/ /  |  $$$$$$/| $$|  $$$$$$/|  $$$$$$/|  $$$$$$$| $$ \  $$|  $$$$$$/
-//   \___/ ________/    \______/ |__/ \______/  \______/  \_______/|__/  \__/ \______/
+// Geometry Visualization Server
+// Copyright (c) 2019 Logan Barnes - All Rights Reserved
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 // ///////////////////////////////////////////////////////////////////////////////////////
 #include "scene_server.hpp"
+
+// project
+#include "gvs/net/type_conversions.hpp"
 
 // external
 #include <grpcpp/server_builder.h>
@@ -24,7 +33,7 @@ namespace gvs::net {
 SceneServer::SceneServer(const std::string& host_address) {
     grpc::ServerBuilder builder;
 
-    builder.RegisterService(&service_);
+    builder.RegisterService(this);
     builder.SetMaxMessageSize(std::numeric_limits<int>::max());
 
     if (!host_address.empty()) {
@@ -35,6 +44,47 @@ SceneServer::SceneServer(const std::string& host_address) {
     //    builder.AddChannelArgument("grpc.so_reuseport", 0);
 
     server_ = builder.BuildAndStart();
+}
+
+grpc::Status SceneServer::AddItem(grpc::ServerContext*                 context,
+                                  const gvs::net::SparseSceneItemInfo* request,
+                                  gvs::net::SceneIdResult*             response) {
+    return Service::AddItem(context, request, response);
+}
+
+grpc::Status SceneServer::UpdateItem(grpc::ServerContext*                       context,
+                                     const gvs::net::SparseSceneItemInfoWithId* request,
+                                     gvs::net::Result*                          response) {
+    return Service::UpdateItem(context, request, response);
+}
+
+grpc::Status SceneServer::AppendToItem(grpc::ServerContext*                       context,
+                                       const gvs::net::SparseSceneItemInfoWithId* request,
+                                       gvs::net::Result*                          response) {
+    return Service::AppendToItem(context, request, response);
+}
+
+grpc::Status SceneServer::GetItemInfo(grpc::ServerContext*     context,
+                                      const gvs::net::SceneId* request,
+                                      gvs::net::SceneItemInfo* response) {
+    return Service::GetItemInfo(context, request, response);
+}
+
+grpc::Status SceneServer::Clear(grpc::ServerContext*             context,
+                                const ::google::protobuf::Empty* request,
+                                ::google::protobuf::Empty*       response) {
+    return Service::Clear(context, request, response);
+}
+
+grpc::Status SceneServer::GetItems(grpc::ServerContext*                               context,
+                                   const ::google::protobuf::Empty*                   request,
+                                   grpc::ServerWriter<gvs::net::SceneItemInfoWithId>* writer) {
+    return Service::GetItems(context, request, writer);
+}
+
+grpc::Status
+SceneServer::SetSeed(grpc::ServerContext* context, const gvs::net::Seed* request, ::google::protobuf::Empty* response) {
+    return Service::SetSeed(context, request, response);
 }
 
 } // namespace gvs::net
--- src/gvs/display/net/scene_server.hpp
+++ src/gvs/display/net/scene_server.hpp
@@ -1,35 +1,73 @@
 // ///////////////////////////////////////////////////////////////////////////////////////
-//                                                                           |________|
-//  Copyright (c) 2020 CloudNC Ltd - All Rights Reserved                        |  |
-//                                                                              |__|
-//        ____                                                                .  ||
-//       / __ \                                                               .`~||$$$$
-//      | /  \ \         /$$$$$$  /$$                           /$$ /$$   /$$  /$$$$$$$
-//      \ \ \ \ \       /$$__  $$| $$                          | $$| $$$ | $$ /$$__  $$
-//    / / /  \ \ \     | $$  \__/| $$  /$$$$$$  /$$   /$$  /$$$$$$$| $$$$| $$| $$  \__/
-//   / / /    \ \__    | $$      | $$ /$$__  $$| $$  | $$ /$$__  $$| $$ $$ $$| $$
-//  / / /      \__ \   | $$      | $$| $$  \ $$| $$  | $$| $$  | $$| $$  $$$$| $$
-// | | / ________ \ \  | $$    $$| $$| $$  | $$| $$  | $$| $$  | $$| $$\  $$$| $$    $$
-//  \ \_/ ________/ /  |  $$$$$$/| $$|  $$$$$$/|  $$$$$$/|  $$$$$$$| $$ \  $$|  $$$$$$/
-//   \___/ ________/    \______/ |__/ \______/  \______/  \_______/|__/  \__/ \______/
+// Geometry Visualization Server
+// Copyright (c) 2019 Logan Barnes - All Rights Reserved
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 // ///////////////////////////////////////////////////////////////////////////////////////
 #pragma once
 
-#include <grpcpp/server.h>
+// project
+#include "gvs/display/display_scene.hpp"
+#include "gvs/util/atomic_data.hpp"
+
+// generated
 #include <scene.grpc.pb.h>
+
+// external
+#include <grpcpp/server.h>
 
 namespace gvs::net {
 
-class SceneServer {
+class SceneServer : public Scene::Service {
 public:
-    using Service = Scene::Service;
-
     explicit SceneServer(std::string const& host_address = "");
 
+    grpc::Status AddItem(grpc::ServerContext*                 context,
+                         const gvs::net::SparseSceneItemInfo* request,
+                         gvs::net::SceneIdResult*             response) override;
+
+    grpc::Status UpdateItem(grpc::ServerContext*                       context,
+                            const gvs::net::SparseSceneItemInfoWithId* request,
+                            gvs::net::Result*                          response) override;
+
+    grpc::Status AppendToItem(grpc::ServerContext*                       context,
+                              const gvs::net::SparseSceneItemInfoWithId* request,
+                              gvs::net::Result*                          response) override;
+
+    grpc::Status GetItemInfo(grpc::ServerContext*     context,
+                             const gvs::net::SceneId* request,
+                             gvs::net::SceneItemInfo* response) override;
+
+    grpc::Status Clear(grpc::ServerContext*             context,
+                       const ::google::protobuf::Empty* request,
+                       ::google::protobuf::Empty*       response) override;
+
+    grpc::Status GetItems(grpc::ServerContext*                               context,
+                          const ::google::protobuf::Empty*                   request,
+                          grpc::ServerWriter<gvs::net::SceneItemInfoWithId>* writer) override;
+
+    grpc::Status
+    SetSeed(grpc::ServerContext* context, const gvs::net::Seed* request, ::google::protobuf::Empty* response) override;
+
 private:
-    Service                       service_;
-    std::unique_ptr<grpc::Server> server_;
+    util::AtomicData<display::DisplayScene> display_scene_;
+    std::unique_ptr<grpc::Server>           server_;
 };
 
 } // namespace gvs::net
